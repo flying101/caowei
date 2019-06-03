15ffce4a1cb94411a1e5b1ae51844e67db3d640a
flying101 <caowei101@sina.com>
2019-06-03 19:17:39 +0800

    fouthcommit

---
+++ database-demo/src/main/java/org/newit/microservice/dao/UserMapper.java
@@ -0,0 +1,10 @@
+package org.newit.microservice.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.newit.microservice.model.User;
+
+@Mapper
+public interface UserMapper {
+    int insertUser(User user);
+    User selectUserById(int userId);
+}
